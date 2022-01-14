ca42b85e1b1a813d0d8004c84f53f895aa140618
y-arimura1222 <y.arimura.carecon@gmail.com>
2022-01-14 21:34:26 +0900

    対話形式のバトル作成

---
+++ Monster.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Monster {
+    private String name;
+    private String attribute;
+    private int hp = 100;
+    private int level = 1;
+    private List<String> skills = new ArrayList<>();
+
+    Scanner scan = new Scanner(System.in);
+
+    Monster () {
+        System.out.println("ポケモンの名前は何ですか？");
+        String pokeName = scan.next();
+        setName(pokeName);
+
+        System.out.println("ポケモンの属性は何ですか？");
+        String pokeAttr = scan.next();
+        setAttribute(pokeAttr);
+
+        System.out.println("技を2種類登録してください:");
+        setSkills();
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public String getName () {
+        return this.name;
+    }
+
+    public void setAttribute (String attribute) {
+        this.attribute = attribute;
+    }
+
+    public void setHp (int hp) {
+        this.hp = hp;
+    }
+
+    public int getHp () {
+        return this.hp;
+    }
+
+    public void setLevel (int level) {
+        this.level = level;
+    }
+
+    public int getLevel () {
+        return this.level;
+    }
+
+    public void setSkills () {
+        for (int i = 1; i <=2; i++) {
+            System.out.println(i + "番目の技は？");
+            String skill = scan.next();
+            this.skills.add(skill);
+        }
+    }
+
+    public List getSkills () {
+        return skills;
+    }
+
+    public void printData () {
+        System.out.println("[ポケモン情報]");
+        System.out.println("名前 : " + this.name + ", 属性 : " + this.attribute + ", 技 : " + skills + ", HP : " + this.hp + ", Level : " + this.level);
+    }
+}
---
+++ UseMonster.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class UseMonster {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Monster インスタンス生成
+        System.out.println("バトルするポケモンを2匹登録してください");
+
+        Monster [] monsters = new Monster[2];
+        for (int i = 0; i < 2; i++) {
+            System.out.println((i + 1) + "匹目のポケモンを登録します");
+            monsters[i] = new Monster();
+            monsters[i].printData();
+        }
+
+        System.out.println(monsters[0].getName() + " vs " + monsters[1].getName() + " バトル開始");
+        System.out.println("----------");
+        System.out.println("----------");
+        Battle battle = new Battle();
+        battle.battle(monsters);
+    }
+}
